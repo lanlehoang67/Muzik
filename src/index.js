2e02ee6fb17ee7a44c29eaf538ff6a2b6f3c28ad
lanlehoang10 <lanlehoang10@gmail.com>
2019-10-21 19:32:32 +0700

    add

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import routes from './routes'
 render(
     <Provider store={store}>
-        <Router routes={routes}></Router>
+        <Router>{routes}</Router>
     </Provider>,
     document.getElementById('root')
 )
