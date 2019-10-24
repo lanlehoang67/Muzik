e1f9296b8ea3b3d67c4929d1154ab403f3851385
lanlehoang10 <lanlehoang10@gmail.com>
2019-10-24 23:26:41 +0700

    fix theme

--- src/containers/App.jsx
+++ src/containers/App.jsx
@@ -4,30 +4,26 @@ import HomePage from './HomePage'
 import ReactJkMusicPlayer from "react-jinke-music-player";
 import "react-jinke-music-player/assets/index.css";
 import {connect} from 'react-redux'
+const options =  {
 
+  audioLists: [
+    
+  ],
+    mode: 'full',
+    theme: 'light',
+    autoPlay: false,
+    showMiniModeCover: false,
+
+}
 class App extends Component {
     render() {
         console.log(this.props.tracks)
-        const options = this.props.tracks == undefined ? null : {
-
-            audioLists: [
-              {
-                name: this.props.tracks[0].title,
-                singer: this.props.tracks[0].artist.name,
-                cover: this.props.tracks[0].picture,
-                musicSrc: this.props.tracks[0].url,
-              },
-            ],
-              theme: 'light',
-              autoPlay: false,
-              showMiniModeCover: false,
-          
-          }
+       
         return (
           <>
             <Menu></Menu>
             <HomePage></HomePage>
-            <ReactJkMusicPlayer {...options} ></ReactJkMusicPlayer>
+            <ReactJkMusicPlayer className="light-theme" {...options} ></ReactJkMusicPlayer>
             </>
         )
     }
